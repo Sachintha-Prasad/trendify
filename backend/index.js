26159ebf6d247c121eed8df9fd25e1aeb9b254d2
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-08-13 13:06:30 +0530

    fix: backend index

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const cors = require("cors")
+const products = require("./lib/products")
 
 // initiallizing the express app
 const app = express()
+// port
+const port = process.env.PORT || 5000
 
 // middlewares
 app.use(express.json())
@@ -12,7 +15,8 @@ app.use(cors())
 app.get("/", (req, res) => {
     res.send("Wellcome to Trendify shop API..")
 })
+app.get("/products", (req, res) => {
+    res.send(products)
+})
 
-// port
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log("server running on port 5000"))
+app.listen(port, () => console.log(`server running on port ${port}`))
