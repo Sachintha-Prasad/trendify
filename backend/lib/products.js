1613f38124022386041b43fc67ff608d7d9c84e2
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-08-13 13:06:17 +0530

    products data

---
+++ backend/lib/products.js
@@ -0,0 +1,58 @@
+const products = [
+    {
+        id: 1,
+        name: "casual tshirt",
+        category: "mens",
+        price: 9800,
+        description:
+            "A versatile casual t-shirt made from soft, breathable fabric. Perfect for everyday wear.",
+        image: "https://res.cloudinary.com/dxl5ddyel/image/upload/v1722617380/men-tshirt_ecgtmq.jpg"
+    },
+    {
+        id: 2,
+        name: "white linen short",
+        category: "mens",
+        price: 19800,
+        description:
+            "Lightweight and stylish white linen shorts, ideal for warm weather and casual outings.",
+        image: "https://res.cloudinary.com/dxl5ddyel/image/upload/v1722617382/men-short_genckb.jpg"
+    },
+    {
+        id: 3,
+        name: "aqualine blouse",
+        category: "womens",
+        price: 12500,
+        description:
+            "An elegant aqualine blouse with a flattering fit, perfect for both casual and formal occasions.",
+        image: "https://res.cloudinary.com/dxl5ddyel/image/upload/v1722617392/women-blouse_st3pxy.jpg"
+    },
+    {
+        id: 4,
+        name: "eqypt traditional top",
+        category: "womens",
+        price: 11100,
+        description:
+            "A beautifully crafted traditional Egyptian top with intricate designs and a comfortable fit.",
+        image: "https://res.cloudinary.com/dxl5ddyel/image/upload/v1722617389/women-blouse-2_fnja4b.jpg"
+    },
+    {
+        id: 5,
+        name: "rainbow hoody",
+        category: "children",
+        price: 5400,
+        description:
+            "A fun and colorful rainbow hoodie for kids, designed to keep them warm and cheerful.",
+        image: "https://res.cloudinary.com/dxl5ddyel/image/upload/v1722617390/child-rainbow-hoody_fgnisb.jpg"
+    },
+    {
+        id: 6,
+        name: "leather jacket",
+        category: "children",
+        price: 6200,
+        description:
+            "A stylish leather jacket for children, providing a rugged look and durable comfort.",
+        image: "https://res.cloudinary.com/dxl5ddyel/image/upload/v1722617384/child-jacket_eshsss.jpg"
+    }
+]
+
+module.exports = products
